7737ffcc64521733fbe77ba7ce2eefc1842190fa
adrmil23daw2 <061091.clot@fje.edu>
2024-05-14 21:02:27 +0200

    dades

--- app/views/M09_Sqlite.js
+++ app/views/M09_Sqlite.js
@@ -2,30 +2,41 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import * as SQLite from 'expo-sqlite';
 
-/**
- * Classe que hereta de Component i que treballa amb el
- * component de SQLite. Les dades es mostren per consola per a facilitar l'exercici
- * @version 1.0 23.03.2020
- * @author sergi.grau@fje.edu
- */
-
 const estils = StyleSheet.create({
   textPeu: {
     color: 'blue',
     fontWeight: 'bold',
     fontSize: 30,
+    backgroundColor: '#FA0'
+  },
+  guerra:{
+    textAlign: 'center',
+    color: 'blue',
+    fontWeight: 'bold',
+    fontSize: 30,
+    backgroundColor: '#FA0',
+    marginBottom: 15,
+  },
+  guerras:{
+    textAlign: 'center',
+    color: 'black',
+    fontWeight: 'bold',
+    fontSize: 20,
+
   },
   peu: {
     paddingTop: 10,
     paddingBottom: 10,
-    backgroundColor: '#FA0'
+
   },
 });
+
 export class M09_Sqlite extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       path: null,
+      items: [],
     };
     db = SQLite.openDatabase("daw2.db");
 
@@ -33,16 +44,23 @@ export class M09_Sqlite extends React.Component {
       tx.executeSql(
         "create table if not exists items (id integer primary key not null, done int, value text);"
       );
+      tx.executeSql("delete from items");
+      tx.executeSql("insert into items (done, value) values (0, ?)", ['Day d, 1945']);
+      tx.executeSql("insert into items (done, value) values (1, ?)", ['Stalingrado, 1400']);
+      tx.executeSql("insert into items (done, value) values (1, ?)", ['Hola,1234']);
+      tx.executeSql("insert into items (done, value) values (1, ?)", ['Hola,1235']);
+
+
     });
+
     console.log('creada taula');
+
     db.transaction(
       tx => {
-        tx.executeSql("insert into items (done, value) values (0, ?)", ['primer']);
-        tx.executeSql("insert into items (done, value) values (1, ?)", ['segon']);
-        tx.executeSql("insert into items (done, value) values (2, ?)", ['tercer']);
-        tx.executeSql("select * from items", [], (_, { rows }) =>
-          console.log(JSON.stringify(rows))
-        );
+        tx.executeSql("select * from items where value in (?, ?, ?, ?)", ['Day d, 1945', 'Stalingrado, 1400', 'Hola,1234','Hola,1235'], (_, { rows }) => {
+          const items = rows._array;
+          this.setState({ items });
+        });
       }
     );
   }
@@ -50,9 +68,11 @@ export class M09_Sqlite extends React.Component {
   render() {
     return (
       <View style={estils.peu}>
-        <Text style={estils.textPeu}> SQLITE </Text>
+        <Text style={estils.guerra}> Guerras </Text>
+        {this.state.items.map((item, index) => (
+          <Text style={estils.guerras} key={index}>{item.value}</Text>
+        ))}
       </View>
     );
-
   }
 }
