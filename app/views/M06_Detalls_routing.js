d145e9a129f3b1677376aab323d06ae1a989e885
adrmil23daw2 <061091.clot@fje.edu>
2024-05-17 03:01:14 +0200

    a

--- app/views/M06_Detalls_routing.js
+++ app/views/M06_Detalls_routing.js
@@ -1,30 +1,76 @@
 import React from 'react';
-import { StyleSheet, Button, Text, View } from 'react-native';
-
+import { StyleSheet, Button, Text, View, Image } from 'react-native';
 
 /**
  * Classe que hereta de Component i que implementa un component
  * independent en l'app. És un component bàsic sense estils
  * Fa servir routing
  * @version 1.0 23.03.2020
- * @author sergi.grau@fje.edu
  */
-
 const estils = StyleSheet.create({
   contenidor: {
     flex: 1,
-    backgroundColor: '#0f0'
+    backgroundColor: '#ffcccc',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  buttonContainer: {
+    marginVertical: 10,
+    width: '80%'
+  },
+  pantallaDetall: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 5,
+    color: '#ffffff',
+    textShadowColor: '#000000',
+    textShadowOffset: { width: 1, height: 1 },
+    textShadowRadius: 1,
+  },
+  button: {
+    backgroundColor: '#ff6666',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 5,
+  },
+  buttonText: {
+    color: '#ffffff',
+    fontSize: 16,
+    textAlign: 'center'
+  },
+  noms: {
+    color: '#ffffff',
+    textShadowColor: '#000000',
+    textShadowRadius: 1,
+    textShadowOffset: { width: 1, height: 1 },
+    fontSize: 16,
+    fontWeight: 'bold',
+    width: 110,
+    textAlign: 'center',
+    justifyContent: 'center'
+
   }
 });
 
 export class M06_Detalls extends React.Component {
     render() {
       return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-          <Text>Pantalla Detall {JSON.stringify(this.props.route.params.nom)}</Text>
-          <Text>el curs és: {JSON.stringify(this.props.route.params.curs)}</Text>
-    
-          <Button title="Tornar a Home" onPress={() => this.props.navigation.navigate('Home')} />
+        <View style={estils.contenidor}>
+          <Text style={estils.pantallaDetall}>
+            Pantalla Detall {JSON.stringify(this.props.route.params.nom)}
+          </Text>
+          <Text style={estils.pantallaDetall}>
+            El curs és: {JSON.stringify(this.props.route.params.curs)}
+          </Text>
+          <View style={estils.buttonContainer}>
+            <Button 
+              title="Tornar a Home" 
+              onPress={() => this.props.navigation.navigate('Home')} 
+              color="#ff6666"/>
+          </View>
+          <Text style={estils.noms}>
+          Alex Maench Arnau Vicente  Adrià Milian
+          </Text>
         </View>
       );
     }
--- app/views/M06_Home_routing.js
+++ app/views/M06_Home_routing.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Button, Text, View } from 'react-native';
+import { View, Text, Button, Image, StyleSheet } from 'react-native';
 
 
 /**
@@ -13,34 +13,82 @@ import { StyleSheet, Button, Text, View } from 'react-native';
 const estils = StyleSheet.create({
   contenidor: {
     flex: 1,
-    backgroundColor: '#0f0'
+    backgroundColor: '#ffcccc',
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingBottom: 100
+
+  },
+  ContBotones: {
+    marginVertical: 10,
+    width: '80%',
+
+  },
+  textoPantallaHome: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+    color: '#ffffff', 
+    textShadowColor: '#000000',
+    textShadowOffset: { width: 1, height: 1 }, 
+    textShadowRadius: 1,
+  },
+  button: {
+    backgroundColor: '#ff6666',
+    paddingVertical: 10, 
+    paddingHorizontal: 20, 
+    borderRadius: 5,
+  },
+  botones: {
+    color: '#ffffff',
+    fontSize: 16,
+    textAlign: 'center'
+    
+  },
+  imagen: {
+    width: 100,
+    height: 100,
   }
 });
 
 export class M06_Home extends React.Component {
-
-  
-    render() {
-      return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-          <Text>Pantalla Home</Text>
-          <Button
-            title="Anar a Detalls"
-            onPress={() => this.props.navigation.navigate('Detall', {
-              nom: 'DAW2',
-              curs: '2024'
-            })}
-          />
-          <Button
-            title="Anar a Camera"
-            onPress={() => this.props.navigation.navigate('Camera')}
-          />
-          <Button
-            title="Anar a SQLite"
-            onPress={() => this.props.navigation.navigate('SQLite')}
-          />
+  render() {
+    return (
+      <View style={estils.contenidor}>
+          <Image source={require('../../assets/img.png')}style={estils.imagen}/>
+        <Text style={estils.textoPantallaHome}>React Native GP2</Text>
+        <View style={estils.ContBotones}>
+          <View style={estils.button}>
+            <Text 
+              style={estils.botones}
+              onPress={() => this.props.navigation.navigate('Detall', {
+                nom: 'DAW2',
+                curs: '2024'
+              })}
+            >Anar a Detalls</Text>
+          </View>
         </View>
-      );
-
-    }
+        <View style={estils.ContBotones}>
+          <View style={estils.button}>
+            <Text 
+              style={estils.botones}
+              onPress={() => this.props.navigation.navigate('Camera')}
+            >
+              Anar a Camera
+            </Text>
+          </View>
+        </View>
+        <View style={estils.ContBotones}>
+          <View style={estils.button}>
+            <Text 
+              style={estils.botones}
+              onPress={() => this.props.navigation.navigate('SQLite')}
+            >
+              Anar a SQLite
+            </Text>
+          </View>
+        </View>
+      </View>
+    );
+  }
 }
